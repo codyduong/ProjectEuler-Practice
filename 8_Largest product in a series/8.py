c5a458980a8cfb562b7298d4a637a3cebb87bdac
Cody-Duong <24193268+Cody-Duong@users.noreply.github.com>
2020-11-02 22:27:40 -0600

    8

---
+++ 8_Largest product in a series/8.py
@@ -0,0 +1,28 @@
+import fileinput
+import math
+
+def product(s):
+    p = 1
+    for c in s:
+        p *= int(c)
+    return p
+
+def ps(n):
+    zero_split = n.split('0') #splits between 0's
+    to_pop = []
+    for i in range(len(zero_split)):
+        if len(zero_split[i])<13:
+            to_pop.append(i)
+
+    for j in range(len(to_pop)):
+        zero_split.pop(to_pop[j]-j)
+
+    current = 0
+    for each in zero_split:
+        current = max(current, max(product(each[i : i + 13]) for i in range(len(each) - 13 + 1)))
+        print(current)
+        
+
+if __name__ == "__main__":
+    i = ''.join([line for line in fileinput.input()]).replace('\n','')
+    print(ps(i))
